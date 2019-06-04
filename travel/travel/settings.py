505e04871219bfe0c50ecedb297acd71ac0bdb4c
krom121 <stephenacgregor9@gmail.com>
2019-06-04 22:52:41 +0100

    basetemp+html+css+floatgird+header

--- travel/travel/settings.py
+++ travel/travel/settings.py
@@ -109,8 +109,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 try:
     from .local_settings import *
